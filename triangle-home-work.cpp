bf610828b79770311d598900dc8adb5a09dc2e8e
mvideo <mvideo@LAPTOP-UBPC7F48>
2024-08-22 22:54:22 +0300

    Добавьте файлы проекта.

---
+++ triangle-home-work.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string layer;
+
+    const char STAR = '*';
+    const char SPACE = ' ';
+
+    int n;
+    std::cout << "Enter the height of the triangle: ";
+    std::cin >> n;
+    if (n < 0) return 0;
+    std::cout << '\n';
+
+    layer += SPACE;
+    for (int i = 0; i <= n; i++){
+        for (int j = i; j <= n - 1; j++)
+        {
+            layer += SPACE;
+        }
+        for (int j = n - i; j < n; j++)
+        {
+            layer += STAR;
+            layer += SPACE;
+        }
+        std::cout << layer << '\n';
+        layer.clear();
+        layer += SPACE;
+    }
+
+    return 0;
+}
